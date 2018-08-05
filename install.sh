a1239495d3132be16555fcc000d00aaf7ed9bdb0
TheHene <hendrik.setzer@hotmail.de>
2018-08-05 18:07:53 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dir=$(pwd)
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in `find . -type f ! -name "install.sh" ! -path "*/.*/*" -exec basename {} \;`; do
+  echo $file
+  mv ~/."$file" ~/."$file"_`date +%s`_bak
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
+done
+
