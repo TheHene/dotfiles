3198ab121a9a52a19b040cf28ece80662e7c0bfa
TheHene <hendrik.setzer@hotmail.de>
2022-12-18 01:20:49 +0100

    more telescope bindings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,10 +1,26 @@
 local builtin = require('telescope.builtin')
+local actions = require "telescope.actions"
 
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>pw', function()
     require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }
 end)
+
+require('telescope').setup{
+   defaults = {
+    mappings = {
+      i = {
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+      }
+    }
+  },
+}
