03c666b437855ff50d78bd3bcddf83fdc4705207
TheHene <hendrik.setzer@hotmail.de>
2022-12-18 01:10:02 +0100

    add telescope mapping

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>pw', function()
+    require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }
 end)
